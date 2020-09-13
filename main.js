7ab65ce48b8b6b6edfd349ac7ce1ffbb16046820
TreeIsLife2 <71183248+TreeIsLife2@users.noreply.github.com>
2020-09-13 15:57:52 +0200

    Created main js

---
+++ main.js
@@ -0,0 +1,27 @@
+// define imports
+var ImportGP;
+var ImportFP;
+var ImportWP;
+
+// define exports
+var ExportGP 
+var ExportFP
+var ExportWP
+
+// define wiki
+var wiki;
+
+// define page
+var page;
+
+
+// Imports
+ImportGP = "https://" + wiki + ".gamepedia.com/Special:Import/" + page;
+ImportFP = "https://" + wiki + ".fandom.com/Special:Import/" + page;
+ImportWP = "https://" + wiki + ".wikia.com/Special:Import/" + page;
+
+
+// Exports
+ExportGP = "https://" + wiki + ".gamepedia.com/Special:Export/" + page;
+ExportFP = "https://" + wiki + ".fandom.com/Special:Export/" + page;
+ExportWP = "https://" + wiki + ".wikia.com/Special:Export/" + page;
